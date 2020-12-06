942ff46093d636c79f3e9c6b0c4782b5d877eaf8
Parm345 <51205078+Parm345@users.noreply.github.com>
2020-12-05 20:18:13 -0500

    file name changed

--- Half-life Simulation.py
+++ Half-life Simulation.py
@@ -3,12 +3,12 @@ import random
 atomsDecayed = 0
 remainingAtoms = 0
 
-numberOfAtoms = int(input("How many atoms are there: ")) # How many atoms in simulation
+numberOfAtoms = int(input("How many atoms are there: ")) # How many atoms in the simulation
 
 for x in range(numberOfAtoms): # Make a random for the amount of atoms the user said there are
     decayed = random.randint(0,1) # the status of atom is here, range from 0 to 1
 
-    if decayed == 0: # of the status of decayed is 0, that means the atom decayed
+    if decayed == 0: # if the status of decayed is 0, that means the atom decayed
         atomsDecayed += 1 # Increase the count of atoms decayed
     else: # if the status of decayed is not 0 (so 1), that atom still reamins
         remainingAtoms += 1 # incrase the count of atoms remaining
